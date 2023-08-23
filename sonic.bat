891910840d0989c274ae494553129fe4f109fd27
art-mtsh <art.mtsh@gmail.com>
2023-08-23 18:04:15 +0300

    c

--- sonic.bat
+++ sonic.bat
@@ -1,6 +1,6 @@
 @echo off
 mode con: cols=120 lines=30
-call W:\TradingBots\venv\Scripts\activate
-cd W:\TradingBots\Sonic\base.py
+call W:\SonicAWS\venv\Scripts\activate
+cd W:\SonicAWS\base.py
 python base.py
 pause
