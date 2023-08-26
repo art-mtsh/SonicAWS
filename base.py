9d4e64a22a7b6df04c01a92c227ea9cd63f8ba17
art-mtsh <art.mtsh@gmail.com>
2023-08-26 14:57:09 +0300

    w

--- base.py
+++ base.py
@@ -55,27 +55,17 @@ def calculation(instr, volume_filter, atr_filter, cloud_filter):
 			# cVolume = df1['cVolume'].to_numpy()
 			
 			# avgvolume_60 = float(((sum(cVolume[-1:-61:-1]) / len(cVolume[-1:-61:-1])) * cClose[-1]) / 1000)
-			atr_24 = (sum(sum([cHigh[-1:-25:-1] - cLow[-1:-25:-1]])) / len(cClose[-1:-25:-1]))
-			atr_24per = atr_24 / (cClose[-1] / 100)
-			atr_24per = float('{:.2f}'.format(atr_24per))
 			
 			sonic = sonic_signal(cHigh=cHigh, cLow=cLow, cClose=cClose, cloud_filter=cloud_filter)
 			
-			if atr_24per != 0:
-				angle_index = sonic[1] / atr_24per
-			else:
-				angle_index = sonic[1] / 1
-				
-			angle_index = float('{:.2f}'.format(angle_index))
-			
-			if atr_24per >= atr_filter: #and avgvolume_60 >= volume_filter
+			if sonic[1] >= atr_filter: #and avgvolume_60 >= volume_filter
 				if '↗️' in sonic[0] or '↘️' in sonic[0]:
-					print(f"{sonic[0]} {symbol}, avg.ATR: {atr_24per}%, angle: {sonic[1]}%, index: {angle_index};")
-					bot1.send_message(662482931, f'{sonic[0]} {symbol}, avg.ATR: {atr_24per}%, angle: {sonic[1]}%, index: {angle_index}')
+					print(f"{sonic[0]} {symbol}, avg.ATR: {sonic[1]}%, index: {sonic[2]};")
+					bot1.send_message(662482931, f'{sonic[0]} {symbol}, avg.ATR: {sonic[1]}%, index: {sonic[2]}')
 				
 				elif '🟢' in sonic[0] or '🔴' in sonic[0]:
 					print(f"---------------------> {symbol};")
-					bot3.send_message(662482931, f'{sonic[0]}{symbol}, avg.ATR: {atr_24per}%, angle: {sonic[1]}%, index: {angle_index}')
+					bot3.send_message(662482931, f'{sonic[0]}{symbol}, avg.ATR: {sonic[1]}%, index: {sonic[2]}')
 					
 		except telebot.apihelper.ApiTelegramException as ex:
 			print(f'Telegram error for {symbol}: {ex}')
--- module_sonic.py
+++ module_sonic.py
@@ -3,21 +3,18 @@ import talipp.indicators.EMA as ema
 
 def sonic_signal(cHigh, cLow, cClose, cloud_filter):
 
+	# INDICATORS
 	ema34_basis = ema(period=34, input_values=cClose)
 	ema34_low = ema(period=34, input_values=cLow)
 	ema34_high = ema(period=34, input_values=cHigh)
 	ema89 = ema(period=89, input_values=cClose)
 	ema233 = ema(period=233, input_values=cClose)
 	
+	# TREND
 	rising_dragon = ema34_low[-1] > ema89[-1] > ema233[-1]
 	falling_dragon = ema34_high[-1] < ema89[-1] < ema233[-1]
 	
-	dragon_distance_k: float
-	if ema34_high[-1] != ema34_low[-1]:
-		dragon_distance_k = abs(ema34_basis[-1] - ema89[-1]) / (ema34_high[-1] - ema34_low[-1])
-	else:
-		dragon_distance_k = 0
-	
+	# CLOUDS
 	cloud_above = 0
 	cloud_below = 0
 	
@@ -28,22 +25,36 @@ def sonic_signal(cHigh, cLow, cClose, cloud_filter):
 	for i in range(2, cloud_filter+2):
 		if cHigh[-i] > ema34_low[-i] or ema34_high[-i] > ema89[-i] or ema89[-i] > ema233[-1]:
 			cloud_below += 1
-			
-			
-	angle = abs(ema34_basis[-1] - ema34_basis[-25]) / (cClose[-1] / 100)
+	
+	# AVERAGE ATR
+	atr = (sum(sum([cHigh[-1:-cloud_filter * 2 - 1:-1] - cLow[-1:-cloud_filter * 2 - 1:-1]])) / len(cClose[-1:-cloud_filter * 2 - 1:-1]))
+	atr_per = atr / (cClose[-1] / 100)
+	atr_per = float('{:.2f}'.format(atr_per))
+	
+	# ANGLE
+	angle = abs(ema34_basis[-1] - ema34_basis[-cloud_filter*2-1]) / (cClose[-1] / 100)
 	angle = float('{:.2f}'.format(angle))
 	
+	# ANGLE COEFFICIENT
+	angle_coef: float
 	
-	if dragon_distance_k > 1:
-		if rising_dragon and cloud_above == 0:
-			if ema34_high[-1] >= cLow[-1] >= ema89[-1]:
-				return ['🟢', angle]
-			return ['↗️', angle]
-		elif falling_dragon and cloud_below == 0:
-			if ema34_low[-1] <= cHigh[-1] <= ema89[-1]:
-				return ['🔴', angle]
-			return ['↘️', angle]
-		else:
-			return ['Sleep', angle]
+	if atr_per != 0:
+		angle_coef = angle / atr_per
 	else:
-		return ['Sleep', angle]
+		angle_coef = angle / 1
+
+	angle_coef = float('{:.2f}'.format(angle_coef))
+	
+	# RESULT
+	if rising_dragon and cloud_above == 0:
+		if ema34_high[-1] >= cLow[-1] >= ema89[-1]:
+			return ['🟢', atr_per, angle_coef]
+		return ['↗️', atr_per, angle_coef]
+	
+	elif falling_dragon and cloud_below == 0:
+		if ema34_low[-1] <= cHigh[-1] <= ema89[-1]:
+			return ['🔴', atr_per, angle_coef]
+		return ['↘️', atr_per, angle_coef]
+	
+	else:
+		return ['Sleep', atr_per, angle_coef]
