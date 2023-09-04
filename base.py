105d5b9eb50eec974233d881934d74560fd66bc2
art-mtsh <art.mtsh@gmail.com>
2023-09-04 16:03:20 +0300

    w

--- base.py
+++ base.py
@@ -131,8 +131,6 @@ def search_activale(price_filter, ticksize_filter, atr_filter, cloud_filter, fir
 								f'{status_message}\n'
 								f'\n'
 								f'🍌 {int(time3)} seconds 🍌')
-	else:
-		bot1.send_message(662482931, f'️😪😪😪')
 	
 	print(f"Finished processes in {int(time3)} seconds, at {datetime.datetime.now().strftime('%H:%M:%S')}\n")
 
