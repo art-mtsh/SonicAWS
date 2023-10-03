e1e57c869ae4330474a810de47ffa656b5d826e4
art-mtsh <art.mtsh@gmail.com>
2023-10-03 22:56:28 +0300

    w

--- spot_divergences/cumulative_delta_spot.py
+++ spot_divergences/cumulative_delta_spot.py
@@ -7,10 +7,8 @@ from module_get_pairs_binance import binance_pairs
 TELEGRAM_TOKEN = '6077915522:AAFuMUVPhw-cEaX4gCuPOa-chVwwMTpsUz8'
 bot1 = telebot.TeleBot(TELEGRAM_TOKEN)
 
-def search(filtered_dictionary):
-	
-	binance_frame = "1h"
-	request_limit_length = 96
+
+def search(filtered_dictionary, binance_frame, request_limit_length):
 	
 	# end_date_timestamp = datetime(2023, 9, 30).timestamp()
 	# end_date = datetime.fromtimestamp(end_date_timestamp)
@@ -21,16 +19,7 @@ def search(filtered_dictionary):
 	# end_date = new_date.timestamp() * 1000
 	
 	for symbol in filtered_dictionary:
-		timestamp = []
-		open = []
-		high = []
-		low = []
-		close = []
-		volume = []
-		buy_volume = []
-		cumulative_delta = []
 
-		# binance_klines = f'https://api.binance.com/api/v3/klines?symbol={symbol}&interval={binance_frame}&limit={request_limit_length}&endTime={int(end_date)}'
 		binance_klines = f'https://api.binance.com/api/v3/klines?symbol={symbol}&interval={binance_frame}&limit={request_limit_length}'
 		binance_klines = requests.get(binance_klines)
 
@@ -45,8 +34,7 @@ def search(filtered_dictionary):
 				close = list(float(i[4]) for i in binance_candle_data)
 				volume = list(float(i[5]) for i in binance_candle_data)
 				buy_volume = list(float(i[9]) for i in binance_candle_data)
-
-				
+			
 				cumulative_delta = {}
 				new_delta = 0
 				
@@ -63,7 +51,7 @@ def search(filtered_dictionary):
 					if low[lowest_low_index] > low[i]:
 						lowest_low_index = i
 				
-				if low[-1] > low[lowest_low_index] and cumulative_delta.get(95) <= cumulative_delta.get(lowest_cd_index):
+				if low[-1] > low[lowest_low_index] and cumulative_delta.get(request_limit_length-1) <= cumulative_delta.get(lowest_cd_index):
 					dist_to_low = (low[-1] - low[lowest_low_index]) / (low[-1] / 100)
 					dist_to_low = '{:.2f}'.format(dist_to_low)
 					print(f"{symbol}: {dist_to_low}%")
@@ -72,19 +60,25 @@ def search(filtered_dictionary):
 		else:
 			print(f"Received status code: {binance_klines.status_code}")
 
+
 if __name__ == '__main__':
 	
 	while True:
+		
+		binance_frame = str(input("Timeframe (1h, 30m, 15m, 5m, 1m): "))
+		request_limit_length = int(input("Request length: "))
+		sleep_time = int(input("Sleep time, minutes: ") * 60)
+		
 		time1 = time.perf_counter()
 		
 		pairs = binance_pairs(4)
 		
 		print(f"pairs: {sum(len(inner_list) for inner_list in pairs)}")
 		
-		t1 = threading.Thread(target=search(pairs[0]))
-		t2 = threading.Thread(target=search(pairs[1]))
-		t3 = threading.Thread(target=search(pairs[2]))
-		t4 = threading.Thread(target=search(pairs[3]))
+		t1 = threading.Thread(target=search(pairs[0], binance_frame, request_limit_length))
+		t2 = threading.Thread(target=search(pairs[1], binance_frame, request_limit_length))
+		t3 = threading.Thread(target=search(pairs[2], binance_frame, request_limit_length))
+		t4 = threading.Thread(target=search(pairs[3], binance_frame, request_limit_length))
 		
 		t1.start()
 		t2.start()
@@ -102,4 +96,5 @@ if __name__ == '__main__':
 		print(f"Finished processes in {int(time3)} seconds")
 		print("")
 		
-		time.sleep(900)
+		time.sleep(sleep_time)
+		
