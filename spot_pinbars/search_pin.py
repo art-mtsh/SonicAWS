974f1de1706827822fc2f7a3b4f0e91ced68893a
art-mtsh <art.mtsh@gmail.com>
2023-10-09 15:23:44 +0300

    w

--- spot_pinbars/search_pin.py
+++ spot_pinbars/search_pin.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
 	print("PARAMETERS:")
 	request_limit_length = 48
 	body_percent_filter = int(input("Body percent (def. 20): ") or 20)
-	pin_close_part = int(input("Close at part (def. 4): ") or 4)
+	pin_close_part = int(input("Close at part (def. 5): ") or 5)
 	pin_range_filter = float(input("Pin range (def. 0.5): ") or 0.5)
 	gap_filter = 0.3
 	tick_size_filter = 0.3
