f6c6f6bcc6def83eab9febce12f7e32535e6452f
art-mtsh <art.mtsh@gmail.com>
2023-10-13 10:47:46 +0300

    w

--- spot_pinbars/screenshoter.py
+++ spot_pinbars/screenshoter.py
@@ -1,6 +1,7 @@
 import telebot
 import matplotlib.pyplot as plt
 from os import remove
+# import requests
 
 # --- TELEGRAM ---
 TELEGRAM_TOKEN = '6077915522:AAFuMUVPhw-cEaX4gCuPOa-chVwwMTpsUz8'
@@ -9,7 +10,8 @@ bot1 = telebot.TeleBot(TELEGRAM_TOKEN)
 
 def screenshoter_send(symbol, cOpen: list, cHigh: list, cLow: list, cClose: list, chart_title):
     # Create a Matplotlib figure for the candlestick chart
-    # fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
+    ax.set_facecolor("#E6E1BE")
 
     for i in range(len(cClose)):
         body_up = cClose[i] - cOpen[i] if cClose[i] != cOpen[i] else cOpen[i] * 0.0001
@@ -17,14 +19,14 @@ def screenshoter_send(symbol, cOpen: list, cHigh: list, cLow: list, cClose: list
         
         if cClose[i] >= cOpen[i]:
             # Up candles
-            plt.bar(x=i, height=body_up, width=0.9, bottom=cOpen[i], color='green')
-            plt.bar(x=i, height=cHigh[i] - cClose[i], width=0.07, bottom=cClose[i], color='green')
-            plt.bar(x=i, height=cOpen[i] - cLow[i], width=0.07, bottom=cLow[i], color='green')
+            plt.bar(x=i, height=body_up, width=0.9, bottom=cOpen[i], color='#008080')
+            plt.bar(x=i, height=cHigh[i] - cClose[i], width=0.07, bottom=cClose[i], color='#008080')
+            plt.bar(x=i, height=cOpen[i] - cLow[i], width=0.07, bottom=cLow[i], color='#008080')
         else:
             # Down candles
-            plt.bar(x=i, height=body_dn, width=0.9, bottom=cClose[i], color='red')
-            plt.bar(x=i, height=cHigh[i] - cOpen[i], width=0.07, bottom=cOpen[i], color='red')
-            plt.bar(x=i, height=cClose[i] - cLow[i], width=0.07, bottom=cLow[i], color='red')
+            plt.bar(x=i, height=body_dn, width=0.9, bottom=cClose[i], color='#696969')
+            plt.bar(x=i, height=cHigh[i] - cOpen[i], width=0.07, bottom=cOpen[i], color='#696969')
+            plt.bar(x=i, height=cClose[i] - cLow[i], width=0.07, bottom=cLow[i], color='#696969')
             
         # grid
         plt.grid(color='grey', linestyle='-', linewidth=0.1)
@@ -39,7 +41,7 @@ def screenshoter_send(symbol, cOpen: list, cHigh: list, cLow: list, cClose: list
     plt.savefig(f'FT{symbol}_{cOpen[-1]}_{cClose[-1]}.png', dpi=400, bbox_inches='tight', pad_inches=0.2)
     pic = open(f'FT{symbol}_{cOpen[-1]}_{cClose[-1]}.png', 'rb')
     bot1.send_photo(662482931, pic)
-    
+
     # CLEANING
     pic.close()
     remove(f'FT{symbol}_{cOpen[-1]}_{cClose[-1]}.png')
@@ -65,4 +67,4 @@ def screenshoter_send(symbol, cOpen: list, cHigh: list, cLow: list, cClose: list
 #         cLow = [float(entry[3]) for entry in binance_candle_data]
 #         cClose = [float(entry[4]) for entry in binance_candle_data]
 #
-#         screenshoter_FrTr(symbol, cOpen, cHigh, cLow, cClose, chart_title="btc\nparameters1\nparameters2\nparameters3")
+#         screenshoter_send(symbol, cOpen, cHigh, cLow, cClose, chart_title="btc\nparameters1\nparameters2\nparameters3")
