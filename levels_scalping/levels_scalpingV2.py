50a27e2843fdcbfab98e6dcdd0e591bc2ac9bbbd
art-mtsh <art.mtsh@gmail.com>
2023-11-01 18:28:44 +0200

    1

--- levels_scalping/levels_scalpingV2.py
+++ levels_scalping/levels_scalpingV2.py
@@ -173,15 +173,14 @@ if __name__ == '__main__':
 	atr_per_filter = 0.20 # float(input("ATR% filter (def. 0.3%): ") or 0.3)
 	trades_k_filter = 100 # int(input("Trades filter (def. 100): ") or 100)
 	
-	bot1.send_message(662482931,
-	                  f"Processes = {proc} \n\n"
-	                  f"Gap filter = {gap_filter}% \n"
-	                  f"Density filter = {density_filter} \n"
-	                  f"Tick size filter = {tick_size_filter}% \n"
-	                  f"ATR% filter = {atr_per_filter}% \n"
-	                  f"Trades = {trades_k_filter}K \n\n"
-	                  f"💵💵💵💵💵"
-					)
+	print(f"Processes = {proc} \n\n"
+          f"Gap filter = {gap_filter}% \n"
+          f"Density filter = {density_filter} \n"
+          f"Tick size filter = {tick_size_filter}% \n"
+          f"ATR% filter = {atr_per_filter}% \n"
+          f"Trades = {trades_k_filter}K \n\n"
+          f"💵💵💵💵💵"
+		)
 	
 	def waiting():
 		
@@ -196,7 +195,7 @@ if __name__ == '__main__':
 			if int(last_second_digit) == 0:
 				break
 
-	while True:
+	if True:
 		
 		manager = Manager()
 		shared_queue = manager.Queue()
