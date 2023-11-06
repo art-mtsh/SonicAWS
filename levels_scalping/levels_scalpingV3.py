1a382bf285465307e1eb00d33c5a4e5c8102543a
art-mtsh <art.mtsh@gmail.com>
2023-11-06 22:20:50 +0200

    1

--- levels_scalping/levels_scalpingV3.py
+++ levels_scalping/levels_scalpingV3.py
@@ -11,15 +11,10 @@ bot1 = telebot.TeleBot(TELEGRAM_TOKEN)
 
 def search(
 		symbol,
-		frame,
-		gap_filter,
-		density_filter,
-		atr_per_filter,
-		trades_k_filter,
-		s_queue,
 		search_distance,
-		levels_scatter
-		):
+		max_minimum_candle,
+		multiplier,
+):
 	
 	while True:
 		
@@ -51,86 +46,63 @@ def search(
 			
 			max_decimal = max([decimal_1, decimal_2, decimal_3])
 		
-			distance_1 = abs(close - combined_list[-1][0]) / (close / 100)
-			distance_2 = abs(close - combined_list[-2][0]) / (close / 100)
-			distance_3 = abs(close - combined_list[-3][0]) / (close / 100)
+			distance_1 = abs(close - combined_list[-1][0]) / (close / 100) if combined_list[-1][1] >= combined_list[-max_minimum_candle][1] * multiplier else 100
+			distance_2 = abs(close - combined_list[-2][0]) / (close / 100) if combined_list[-2][1] >= combined_list[-max_minimum_candle][1] * multiplier else 100
+			distance_3 = abs(close - combined_list[-3][0]) / (close / 100) if combined_list[-3][1] >= combined_list[-max_minimum_candle][1] * multiplier else 100
 			
 			distance_1 = float('{:.2f}'.format(distance_1))
 			distance_2 = float('{:.2f}'.format(distance_2))
 			distance_3 = float('{:.2f}'.format(distance_3))
-			
-			# print(f"{symbol}: {distance_1}, {distance_2}, {distance_3}")
-			
-			search_distance = 1.0
-			max_minimum_candle = 4
-			multiplier = 2
+
 			
 			if min([distance_1, distance_2, distance_3]) <= search_distance:
 				
 				minimum_dist = min([distance_1, distance_2, distance_3])
 				
-				if distance_1 == minimum_dist and \
-					combined_list[-1][1] >= combined_list[-max_minimum_candle][1] * multiplier:
+				if minimum_dist <= search_distance / 4:
+					dist_marker = "🟩"
+				elif minimum_dist <= search_distance / 4 * 2:
+					dist_marker = "🟨"
+				elif minimum_dist <= search_distance / 4 * 3:
+					dist_marker = "🟧"
+				else:
+					dist_marker = "🟥"
+					
+				if distance_1 == minimum_dist:
 					
 					decimal_x = len(str(combined_list[-1][0]).split('.')[-1].rstrip('0'))
-					
-					if max_decimal - decimal_x > 1:
-					
-						msg = f"\n{symbol}: {combined_list[-1][0]} {decimal_x}/{max_decimal} {int(combined_list[-1][1])} >= {int(combined_list[-max_minimum_candle][1])} in {distance_1}%"
-						print(msg)
-						bot1.send_message(662482931, msg)
+
+					msg = f"\n{dist_marker} {symbol}: {combined_list[-1][0]} ({decimal_x}/{max_decimal}), {int(combined_list[-1][1] / 1000)}K > {int(combined_list[-max_minimum_candle][1] / 1000)}K in {distance_1}%"
+					print(msg)
+					bot1.send_message(662482931, msg)
 	
-				elif distance_2 == minimum_dist and \
-					combined_list[-2][1] >= combined_list[-max_minimum_candle][1] * multiplier:
+				elif distance_2 == minimum_dist:
 					
 					decimal_x = len(str(combined_list[-2][0]).split('.')[-1].rstrip('0'))
-					
-					if max_decimal - decimal_x > 1:
-					
-						msg = f"\n{symbol}: {combined_list[-2][0]} {decimal_x}/{max_decimal} {int(combined_list[-2][1])} >= {int(combined_list[-max_minimum_candle][1])} in {distance_2}%"
-						print(msg)
-						bot1.send_message(662482931, msg)
-					
-				elif distance_3 == minimum_dist and \
-					combined_list[-3][1] >= combined_list[-max_minimum_candle][1] * multiplier:
+				
+					msg = f"\n{dist_marker} {symbol}: {combined_list[-2][0]} ({decimal_x}/{max_decimal}), {int(combined_list[-2][1] / 1000)}K > {int(combined_list[-max_minimum_candle][1] / 1000)}K in {distance_2}%"
+					print(msg)
+					bot1.send_message(662482931, msg)
+				
+				elif distance_3 == minimum_dist:
 					
 					decimal_x = len(str(combined_list[-3][0]).split('.')[-1].rstrip('0'))
 					
-					if max_decimal - decimal_x > 1:
-						
-						msg = f"\n{symbol}: {combined_list[-3][0]} {decimal_x}/{max_decimal} {int(combined_list[-3][1])} >= {int(combined_list[-max_minimum_candle][1])} in {distance_3}%"
-						print(msg)
-						bot1.send_message(662482931, msg)
+					msg = f"\n{dist_marker} {symbol}: {combined_list[-3][0]} ({decimal_x}/{max_decimal}), {int(combined_list[-3][1] / 1000)}K > {int(combined_list[-max_minimum_candle][1] / 1000)}K in {distance_3}%"
+					print(msg)
+					bot1.send_message(662482931, msg)
 					
 				else:
-					print(".", end="")
+					print(".")
 			else:
-				print(".", end="")
-				
-		time.sleep(2)
+				print(".")
+		time.sleep(1)
 		
 if __name__ == '__main__':
-	
-	proc = 14
-	gap_filter = 0.5 # float(input("Max gap filter (def. 0.2%): ") or 0.2)
-	density_filter = 25 # int(input("Density filter (def. 30): ") or 30)
-	tick_size_filter = 0.1 # float(input("Ticksize filter (def. 0.03%): ") or 0.03)
-	atr_per_filter = 0.20 # float(input("ATR% filter (def. 0.3%): ") or 0.3)
-	trades_k_filter = 100 # int(input("Trades filter (def. 100): ") or 100)
+
 	search_distance = 1.0
-	levels_scatter = 0.03 / 100
-	
-	# def waiting():
-	#
-	# 	while True:
-	# 		now = datetime.now()
-	# 		last_hour_digit = int(now.strftime('%H'))
-	# 		last_minute_digit = now.strftime('%M')
-	# 		last_second_digit = now.strftime('%S')
-	# 		time.sleep(1)
-	#
-	# 		if int(last_second_digit) == 0:
-	# 			break
+	max_minimum_candle = 4
+	multiplier = 2
 
 	if True:
 		
@@ -138,7 +110,7 @@ if __name__ == '__main__':
 		shared_queue = manager.Queue()
 
 		time1 = time.perf_counter()
-		pairs = ['GASUSDT', 'NEOUSDT', 'ARKUSDT', 'XVSUSDT', 'GMTUSDT']
+		pairs = ['BLURUSDT', 'GMTUSDT', 'XVSUSDT', 'GASUSDT', 'TWTUSDT', "YGGUSDT"]
 		
 		print(f">>> {datetime.now().strftime('%H:%M:%S')}")
 		
@@ -147,14 +119,9 @@ if __name__ == '__main__':
 			process = Process(target=search,
 			                  args=(
 				                  pair,
-			                      "1m",
-			                      gap_filter,
-			                      density_filter,
-			                      atr_per_filter,
-				                  trades_k_filter,
-			                      shared_queue,
 				                  search_distance,
-				                  levels_scatter,
+				                  max_minimum_candle,
+				                  multiplier,
 			                      ))
 			the_processes.append(process)
 
@@ -174,5 +141,4 @@ if __name__ == '__main__':
 
 		# print(f"<<< {datetime.now().strftime('%H:%M:%S')} / {int(time3)} seconds")
 		print("Process ended.")
-		# waiting()
 		
