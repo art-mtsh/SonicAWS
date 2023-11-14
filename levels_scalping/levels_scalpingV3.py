748c0db11c98f8d6a6b05f0f1769e94aea658f45
art-mtsh <art.mtsh@gmail.com>
2023-11-14 11:44:18 +0200

    1

--- levels_scalping/levels_scalpingV3.py
+++ levels_scalping/levels_scalpingV3.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
 		manager = Manager()
 		shared_queue = manager.Queue()
 		
-		print(f">>> {datetime.now().strftime('%H:%M:%S')} {len(pairs)} pairs, {int(reload_time + 2.5)} reload period")
+		print(f"START at {datetime.now().strftime('%H:%M:%S')}, {len(pairs)} pairs, {int(reload_time + 2.5)} s. reload period")
 
 		the_processes = []
 		for pair in pairs:
