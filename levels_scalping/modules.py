50a68209d9706d090959472f94bafb492de2a4ab
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2023-12-19 15:38:20 +0200

    1

--- levels_scalping/modules.py
+++ levels_scalping/modules.py
@@ -111,14 +111,14 @@ def three_distances(symbol, close, combined_list, max_avg_size, search_distance,
 	if size_1 >= max_avg_size and size_1_dollars >= avg_vol_60 and distance_1 <= search_distance:
 
 		for i in range(2, len(high)-6):
-			if high[-i] >= max(high[-1:-i-5:-1]) and high[-1:-i-5:-1].count(high[-i]) >= 2 and price_1 * 0.999 <= high[-i] <= price_1:
+			if high[-i] >= max(high[-1:-i-5:-1]) and high[-1:-i-5:-1].count(high[-i]) >= 1 and price_1 * 0.999 <= high[-i] <= price_1:
 
 				print(f"{symbol} {market_type.capitalize()} {-i} candles ago was bounce at price {high[-i]}, current size {size_1}")
 				res.append([distance_1, price_1, size_1])
 				break
 
 		for i in range(2, len(low)-6):
-			if low[-i] <= min(low[-1:-i-5:-1]) and low[-1:-i-5:-1].count(low[-i]) >= 2 and price_1 <= low[-i] <= price_1 * 1.001:
+			if low[-i] <= min(low[-1:-i-5:-1]) and low[-1:-i-5:-1].count(low[-i]) >= 1 and price_1 <= low[-i] <= price_1 * 1.001:
 
 				print(f"{symbol} {market_type.capitalize()} {-i} candles ago was bounce at price {low[-i]}, current size {size_1}")
 				res.append([distance_1, price_1, size_1])
@@ -127,14 +127,14 @@ def three_distances(symbol, close, combined_list, max_avg_size, search_distance,
 	if size_2 >= max_avg_size and size_2_dollars >= avg_vol_60 and distance_2 <= search_distance:
 
 		for i in range(2, len(high)-6):
-			if high[-i] >= max(high[-1:-i - 5:-1]) and high[-1:-i-5:-1].count(high[-i]) >= 2 and price_2 * 0.999 <= high[-i] <= price_2:
+			if high[-i] >= max(high[-1:-i - 5:-1]) and high[-1:-i-5:-1].count(high[-i]) >= 1 and price_2 * 0.999 <= high[-i] <= price_2:
 
 				print(f"{symbol} {market_type.capitalize()} {-i} candles ago was bounce at price {high[-i]}, current size {size_1}")
 				res.append([distance_2, price_2, size_2])
 				break
 
 		for i in range(2, len(low)-6):
-			if low[-i] <= min(low[-1:-i - 5:-1]) and low[-1:-i-5:-1].count(low[-i]) >= 2 and price_2 <= low[-i] <= price_2 * 1.001:
+			if low[-i] <= min(low[-1:-i - 5:-1]) and low[-1:-i-5:-1].count(low[-i]) >= 1 and price_2 <= low[-i] <= price_2 * 1.001:
 
 				print(f"{symbol} {market_type.capitalize()} {-i} candles ago was bounce at price {low[-i]}, current size {size_1}")
 				res.append([distance_2, price_2, size_2])
@@ -144,14 +144,14 @@ def three_distances(symbol, close, combined_list, max_avg_size, search_distance,
 	if size_3 >= max_avg_size and size_3_dollars >= avg_vol_60 and distance_3 <= search_distance:
 
 		for i in range(2, len(high)-6):
-			if high[-i] >= max(high[-1:-i - 5:-1]) and high[-1:-i-5:-1].count(high[-i]) >= 2 and price_3 * 0.999 <= high[-i] <= price_3:
+			if high[-i] >= max(high[-1:-i - 5:-1]) and high[-1:-i-5:-1].count(high[-i]) >= 1 and price_3 * 0.999 <= high[-i] <= price_3:
 
 				print(f"{symbol} {market_type.capitalize()} {-i} candles ago was bounce at price {high[-i]}, current size {size_1}")
 				res.append([distance_3, price_3, size_3])
 				break
 
 		for i in range(2, len(low)-6):
-			if low[-i] <= min(low[-1:-i - 5:-1]) and low[-1:-i-5:-1].count(low[-i]) >= 2 and price_3 <= low[-i] <= price_3 * 1.001:
+			if low[-i] <= min(low[-1:-i - 5:-1]) and low[-1:-i-5:-1].count(low[-i]) >= 1 and price_3 <= low[-i] <= price_3 * 1.001:
 
 				print(f"{symbol} {market_type.capitalize()} {-i} candles ago was bounce at price {low[-i]}, current size {size_1}")
 				res.append([distance_3, price_3, size_3])
