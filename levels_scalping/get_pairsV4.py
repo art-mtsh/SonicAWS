f1ff0b05dba4e637e0676114511cf965e5fc40c3
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-01-12 22:01:31 +0200

    1

--- levels_scalping/get_pairsV4.py
+++ levels_scalping/get_pairsV4.py
@@ -11,21 +11,17 @@ trades daily
 
 """
 
-excluded = ["USTCUSDT"]
+# excluded = ["USTCUSDT"]
+excluded = []
+
 
 def calculate(dict_of_pairs,
-              x_range_filter,
-              x_change_filter,
-              x_volume_filter,
-              x_trades_filter,
-              x_atr_per_filter,
-              ts_percent_filter,
               shared_queue,
               ):
-    
+
     request_limit_length = 240
     frame = '1m'
-    
+
     for symbol, ts in dict_of_pairs.items():
         futures_klines = f'https://fapi.binance.com/fapi/v1/klines?symbol={symbol}&interval={frame}&limit={request_limit_length}'
         # spot_klines = f'https://api.binance.com/api/v3/klines?symbol={symbol}&interval={frame}&limit={request_limit_length}'
@@ -42,45 +38,38 @@ def calculate(dict_of_pairs,
                 trades = list(int(i[8]) for i in binance_candle_data)
                 buy_volume = list(float(i[9]) for i in binance_candle_data)
                 sell_volume = [volume[0] - buy_volume[0]]
-    
+
                 x_range = (max(high) - min(low)) / (close[-1] / 100)
                 x_range = float('{:.2f}'.format(x_range))
-    
+
                 x_change = (close[-1] - close[0]) / (close[-1] / 100)
                 x_change = float('{:.2f}'.format(x_change))
-    
+
                 x_volume = sum(volume) / 1000000
                 x_volume = float('{:.2f}'.format(x_volume))
-    
+
                 x_trades = int(sum(trades) / 1000)
-    
+
                 x_atr_per = [(high[-c] - low[-c]) / (close[-c] / 100) for c in range(request_limit_length)]
                 x_atr_per = float('{:.2f}'.format(sum(x_atr_per) / len(x_atr_per)))
-    
+
                 ts_percent = float(ts) / (close[-1] / 100)
                 ts_percent = float('{:.4f}'.format(ts_percent))
-                
-                if x_range >= x_range_filter and \
-                    x_change >= x_change_filter and \
-                    x_volume >= x_volume_filter and \
-                    x_trades >= x_trades_filter and \
-                    x_atr_per >= x_atr_per_filter and \
-                    ts_percent <= ts_percent_filter and \
-                    symbol not in excluded:
-                    
-                    print(f"{symbol}, "
-                          f"range {x_range}, "
-                          f"change {x_change}, "
-                          f"volume {x_volume}, "
-                          f"trades {x_trades}, "
-                          f"avg ATR {x_atr_per}, "
-                          f"ticksize {ts_percent}")
+
+                shared_queue.put([symbol, ts_percent, x_trades, x_atr_per])
+                # if ts_percent <= 0.05 and x_atr_per >= 0.25:
+                    # print(f"{symbol}, "
+                    #       f"range {x_range}, "
+                    #       f"change {x_change}, "
+                    #       f"volume {x_volume}, "
+                    #       f"trades {x_trades}, "
+                    #       f"avg ATR {x_atr_per}, "
+                    #       f"ticksize {ts_percent}")
                     # print(symbol, end=", ")
-                    shared_queue.put(symbol)
-                
-                # if symbol == 'BTCUSDT':
-                #     print(f" ======>>> {symbol} volatility: {daily_volatility}%, day change: {daily_change}%, volume: {daily_volume}M, trades: {daily_trades}K, atr5m: {avg_atr_per}%, ticksize: {ts_percent}%")
-                    
+                    # shared_queue.put([symbol, x_trades])
+                # else:
+                #     print(f"Else {symbol} ts: {ts_percent}%, atr: {x_atr_per}%")
+
 def split_dict(input_dict, num_parts):
     avg = len(input_dict) // num_parts
     remainder = len(input_dict) % num_parts
@@ -95,17 +84,12 @@ def split_dict(input_dict, num_parts):
     return result
 
 
-def get_pairs(x_range_filter,
-              x_change_filter,
-              x_volume_filter,
-              x_trades_filter,
-              x_atr_per_filter,
-              ts_percent_filter,):
-    
+def get_pairs():
+
     ts_dict = {}
-    
+
     futures_exchange_info_url = "https://fapi.binance.com/fapi/v1/exchangeInfo"
-    
+
     response = requests.get(futures_exchange_info_url)
     response_data = response.json()
     response_data = response_data.get("symbols")
@@ -116,9 +100,9 @@ def get_pairs(x_range_filter,
         quoteAsset = data['quoteAsset']
         if quoteAsset == "USDT":
             ts_dict.update({symbol: tick_size})
-    
+
     list_of_dicts = split_dict(ts_dict, 16)
-    
+
     manager = Manager()
     shared_queue = manager.Queue()
 
@@ -127,38 +111,29 @@ def get_pairs(x_range_filter,
         process = Process(target=calculate,
                           args=(
                               dict_of_pairs,
-                              x_range_filter,
-                              x_change_filter,
-                              x_volume_filter,
-                              x_trades_filter,
-                              x_atr_per_filter,
-                              ts_percent_filter,
                               shared_queue,
                           ))
         the_processes.append(process)
-    
+
     for pro in the_processes:
         pro.start()
-    
+
     for pro in the_processes:
         pro.join()
-    
+
     pairs = []
     while not shared_queue.empty():
         pairs.append(shared_queue.get())
-    
+
     for pro in the_processes:
         pro.close()
-    
-    return pairs
+
+    sorted_res = [inner_list for inner_list in pairs if inner_list[1] <= 0.05 and inner_list[3] >= 0.25]
+    sorted_res = sorted(sorted_res, key=lambda x: x[3], reverse=True)
+    sorted_res = [inner_list[0] for inner_list in sorted_res[:20]]
+
+    return sorted_res
 
 
 # if __name__ == '__main__':
-#     print(get_pairs(
-#         x_range_filter = 0,
-#         x_change_filter = 0,
-#         x_volume_filter = 0,
-#         x_trades_filter = 0,
-#         x_atr_per_filter = 0.25,
-#         ts_percent_filter = 0.1
-#     ))
+#     print(get_pairs())
