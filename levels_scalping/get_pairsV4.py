16af244012868cec2b2603d259eabf7abd79d9d4
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-01-13 13:25:25 +0200

    1

--- levels_scalping/get_pairsV4.py
+++ levels_scalping/get_pairsV4.py
@@ -143,7 +143,7 @@ def get_pairs():
     msg = (f"We have {total_sorted} pairs in total\n"
            f"(tick < 0.05%, ATR > 0.25)\n\n"
            f"{pairs_to_message} \n\n"
-           f"Got to work {work_quantity} pairs...")
+           f"{work_quantity} pairs taken to calculate...")
 
     bot1.send_message(662482931, msg)
 
